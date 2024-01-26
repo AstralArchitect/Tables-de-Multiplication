79435705e0ec36922a31e076d17810b8eefb45cb
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-01-26 20:25:40 +0100

    update

--- app/src/main/java/fr/matthsudio/tablesdemultiplication/MainActivity.kt
+++ app/src/main/java/fr/matthsudio/tablesdemultiplication/MainActivity.kt
@@ -1,26 +1,34 @@
 package fr.matthsudio.tablesdemultiplication
 
 import android.os.Bundle
-import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContentProviderCompat.requireContext
 import fr.matthsudio.tablesdemultiplication.ui.theme.TablesDeMultiplicationTheme
-import java.io.File
-import java.io.FileWriter
-import java.io.IOException
-import java.io.PrintWriter
+
 
 var note = 0;
-var a = 1;
+var a = 0;
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +46,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
 fun PoserQuestion() {
     var rep by remember { mutableStateOf(TextFieldValue()) }
@@ -64,34 +71,37 @@ fun PoserQuestion() {
                     .padding(bottom = 16.dp)
             )
         }
-        Button(
-            onClick = {
-                if (a != 10) {
-                    if (afficherQuestion) {
-                        afficherResultat = true
-                        afficherQuestion = false // Désactiver l'affichage de la question après avoir cliqué sur Valider
-                        if (rep.text == resultat.toString()) {
-                            note++
+        if (a != 10 && afficherQuestion) {
+            Button(
+                onClick = {
+                    if (a != 10) {
+                        if (afficherQuestion) {
+                            afficherResultat = true
+                            afficherQuestion =
+                                false // Désactiver l'affichage de la question après avoir cliqué sur Valider
+                            if (rep.text == resultat.toString()) {
+                                note++
+                            }
                         }
+                        afficherResultat = false
+                        afficherQuestion = true
+                        prem = (2..10).random()
+                        deux = (2..10).random()
+                        resultat = prem * deux
+                        rep = TextFieldValue()
+                        a++
+                    } else {
+                        afficherResultat = false
+                        afficherQuestion = true
                     }
-                    afficherResultat = false
-                    afficherQuestion = true
-                    prem = (2..10).random()
-                    deux = (2..10).random()
-                    resultat = prem * deux
-                    rep = TextFieldValue()
-                    a++
-                } else {
-                    afficherResultat = false
-                    afficherQuestion = true
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            ) {
+                if (afficherQuestion) {
+                    Text("Valider")
                 }
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        ) {
-            if (afficherQuestion) {
-                Text("Valider")
             }
         }
         if (a == 10){
@@ -103,14 +113,46 @@ fun PoserQuestion() {
 @Composable
 fun fin(modifier: Modifier = Modifier) {
     if (a == 10) {
-        Surface(color = Color.Unspecified) {
-            Text(
-                text = "Vous avez eu " + note + "/10 ! Vous pouvez fermer l'application.",
-                modifier = Modifier.padding(25.dp)
-            )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top
+        ) {
+            if (note <= 6) {
+                Surface(color = Color.Unspecified) {
+                    Text(
+                        text = "Vous avez eu " + note + "/10 ! Il va faloir réviser vos tables de multiplication !",
+                        modifier = Modifier.padding(25.dp)
+                    )
+                }
+            }else if (note >= 7 && note != 10) {
+                Surface(color = Color.Unspecified) {
+                    Text(
+                        text = "Vous avez eu " + note + "/10 ! Très bien, mais vous pouvez faire encore mieux.",
+                        modifier = Modifier.padding(25.dp)
+                    )
+                }
+            }else if (note == 10) {
+                Surface(color = Color.Unspecified) {
+                    Text(
+                        text = "Vous avez eu " + note + "/10! Exellent, vous êtes un pros des tables de multiplications !",
+                        modifier = Modifier.padding(25.dp)
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {
+                    System.exit(0)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            ) {
+                Text("Terminer")
+            }
         }
     }
-
 }
-
 
