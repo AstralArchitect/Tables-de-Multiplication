6416b3c00f894b803ee78eadf3e64098ce689e0b
Astral Architect <matthieu.jlp.poncet@gmail.com>
2024-12-08 12:18:38 +0100

    update algo

--- app/src/main/java/fr/matthsudio/tablesdemultiplication/MainActivity.kt
+++ app/src/main/java/fr/matthsudio/tablesdemultiplication/MainActivity.kt
@@ -1,170 +1,145 @@
 package fr.matthsudio.tablesdemultiplication
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.dp
-import fr.matthsudio.tablesdemultiplication.ui.theme.TablesDeMultiplicationTheme
 
+class Question(one: Int, two: Int){
+    var fact1: Int = one
+    var fact2: Int = two
+    var res: Int = fact1 * fact2
+    var answer: Int = 0
+}
+class Progression{
+    private var note: Int = 0
+    private var progression: Int = 0
+    private var mauvaiseReponses: MutableList<Question> = mutableListOf()
+    var cAnswerList: MutableList<Int> = mutableListOf()
+    var actualQuestion: Question = Question(0,0)
 
-var note = 0;
-var progression = 0;
-var liste = mutableListOf(0);
-var table = 0;
+    fun processQuestion(answer: Int) {
+        mauvaiseReponses.add(actualQuestion)
+    }
+    fun resetMauvaiseReponses() {
+        mauvaiseReponses = mutableListOf()
+    }
+    fun getMauvaiseReponses(): List<Question> {
+        return mauvaiseReponses
+    }
+
+    fun progress() {
+        progression++
+    }
+    fun resetProgression() {
+        progression = 0
+    }
+    fun getProgression(): Int {
+        return progression
+    }
+
+    fun getNote(): Int {
+        return note
+    }
+    fun resetNote() {
+        note = 0
+    }
+    fun incNote() {
+        this.note++
+    }
+}
+
 class MainActivity : ComponentActivity() {
+    private val userProg = Progression()
+    private lateinit var textView: TextView
+    private lateinit var nextButton: Button
+    private lateinit var restartButton: Button
+    private lateinit var answerEditText: EditText
+
+    // changer la plage des tables, actuellement de 2 à 10
+    private val range = 2..10
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TablesDeMultiplicationTheme {
-                // Une surface en utilisant la couleur de fond du thème
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    TextField(
-                            value = rep,
-                            onValueChange = { rep = it },
-                            label = { Text("Quel table de multiplication voulez-vous réviser ?") },
-                            modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(bottom = 16.dp)
-                    )
-                    PoserQuestion()
-                }
+        setContentView(R.layout.activity_main)
+        // init les boutons et autres
+        textView = findViewById(R.id.commentText)
+        nextButton = findViewById(R.id.nextButton)
+        answerEditText = findViewById(R.id.answerEditText)
+        restartButton = findViewById(R.id.restartButton)
+        // poser une question à l'utilisateur
+        val question = ask(range)
+        userProg.actualQuestion = question
+        answerEditText.hint = "Combien font ${question.fact1} x ${question.fact2} ?"
+    }
+    @SuppressLint("SetTextI18n")
+    fun onButtonClicked(view: View) {
+        try {
+            val answer = answerEditText.text.toString().toInt()
+            userProg.progress()
+
+            if (answer == userProg.actualQuestion.res) {
+                userProg.incNote()
             }
+            else{
+                userProg.actualQuestion.answer = answer
+                userProg.processQuestion(answer)
+            }
+            answerEditText.text.clear()
+
+            if (userProg.getProgression() >= 10) {
+                var str = "Vous avez obtenu ${userProg.getNote()}/10\n\nVos erreurs :\n"
+                for (i in userProg.getMauvaiseReponses()) {
+                    str += "${i.fact1} x ${i.fact2} = ${i.res} (votre réponse : ${i.answer})\n"
+                }
+                textView.text = str
+                nextButton.visibility = View.GONE
+                answerEditText.visibility = View.GONE
+                restartButton.visibility = View.VISIBLE
+                return
+            }
+
+            val question = ask(range)
+            userProg.actualQuestion = question
+            answerEditText.hint = "Combien font ${question.fact1} x ${question.fact2} ?"
+            textView.text = ""
+        } catch (e: NumberFormatException) {
+            textView.text = "Veuillez entrer un nombre"
+            return
         }
     }
-}
-@Composable
-fun PoserQuestion() {
-    var prem by remember { mutableStateOf((2..10).random()) }
-    var rep by remember { mutableStateOf(TextFieldValue()) }
-    var afficherResultat by remember { mutableStateOf(false) }
-    var afficherQuestion by remember { mutableStateOf(true) }
-    var deux by remember { mutableStateOf((2..10).random()) }
-    var resultat by remember { mutableStateOf(prem * deux) }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        // Afficher la question seulement si afficherQuestion est vrai
-        if (progression != 10 && afficherQuestion) {
-            TextField(
-                value = rep,
-                onValueChange = { rep = it },
-                label = { Text("Combien font $prem x $deux ?") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            )
-        }
-        if (progression != 10 && afficherQuestion) {
-            Button(
-                onClick = {
-                    if (progression != 10) {
-                        if (afficherQuestion) {
-                            afficherResultat = true
-                            //afficherQuestion = false // Désactiver l'affichage de la question après avoir cliqué sur Valider
-                            if (rep.text == resultat.toString()) {
-                                note++
-                            }
-                        }
-                        afficherResultat = false
-                        afficherQuestion = true
-                        deux = (2..10).random()
-                        prem = (2..10).random()
-                        resultat = prem * deux
-                        while (liste.contains(resultat)){
-                            deux = (2..10).random()
-                            resultat = prem * deux;
-                        }
-                        liste.add(resultat);
-                        rep = TextFieldValue()
-                        progression++;
-                    } else {
-                        afficherResultat = false
-                        afficherQuestion = true
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            ) {
-                if (afficherQuestion) {
-                    Text("Valider")
-                }
-            }
-        }
-        if (progression == 10){
-            fin()
-        }
+    private fun ask(range: IntRange): Question {
+        var one: Int
+        var two: Int
+
+        do {
+            one = range.random()
+            two = range.random()
+        } while (userProg.cAnswerList.contains(one * two))
+        userProg.cAnswerList.add(one * two)
+
+        return Question(one, two)
+    }
+
+    fun onRestartButtonClicked(view: View) {
+        val intent = intent
+        finish()
+        startActivity(intent)
     }
 }
 
-@Composable
-fun fin(modifier: Modifier = Modifier) {
-    if (progression == 10) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Top
-        ) {
-            if (note <= 6) {
-                Surface(color = Color.Unspecified) {
-                    Text(
-                        text = "Vous avez eu " + note + "/10 ! Il va faloir réviser vos tables de multiplication !",
-                        modifier = Modifier.padding(25.dp)
-                    )
-                }
-            }else if (note >= 7 && note != 10) {
-                Surface(color = Color.Unspecified) {
-                    Text(
-                        text = "Vous avez eu " + note + "/10 ! Très bien, mais vous pouvez faire encore mieux.",
-                        modifier = Modifier.padding(25.dp)
-                    )
-                }
-            }else if (note == 10) {
-                Surface(color = Color.Unspecified) {
-                    Text(
-                        text = "Vous avez eu " + note + "/10! Exellent, vous êtes un pros des tables de multiplications !",
-                        modifier = Modifier.padding(25.dp)
-                    )
-                }
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = {
-                    System.exit(0)
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            ) {
-                Text("Terminer")
-            }
-        }
-    }
+// TODO: changer la couleur du theme
+/*fun getThemeColor(context: Context, attribute: Int): Int {
+    val typedValue = TypedValue()
+    context.theme.resolveAttribute(attribute, typedValue, true)
+    return typedValue.data
 }
+// Usage:
+val primaryColor = getThemeColor(this, android.R.attr.colorPrimary)*/
